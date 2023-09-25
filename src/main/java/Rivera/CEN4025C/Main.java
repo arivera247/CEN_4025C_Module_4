0220e4726bfea67f1e549fd7d30e4594bbe9130c
arivera247 <9136079+arivera247@users.noreply.github.com>
2023-09-24 22:09:29 -0400

    Implemented LinkedList method using same procedure as ArrayList method.

--- src/main/java/Rivera/CEN4025C/Main.java
+++ src/main/java/Rivera/CEN4025C/Main.java
@@ -8,7 +8,7 @@ Description:
 
 Write a Java application that does the following. The Main method should:
 
-Call a new method which adds 2,000,000 random integers into an ArrayList, then deletes each one from the ArrayList
+Call a new method which adds 2,000,000 random integers into an ArrayList, then deletes each one from the ArrayList (Done)
 
 Call a new method which adds 2,000,000 random integers into a LinkedList, then deletes each one from the LinkedList
 
@@ -27,6 +27,7 @@ You will have to include the answer this question: Which method had the longest 
  */
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 //Prerequisites: Before writing the code below, I installed JProfiler 14.0 as discussed in the Profiling with JProfiler [1] and JProfiler’s integration into IntelliJ IDEA [2] videos.
@@ -43,6 +44,8 @@ public class Main {
 
         //Declaring the ArrayList
         ArrayList<Integer> myArray = new ArrayList<Integer>();
+        //Reference [3]
+
         //New method for random integers
         Random randNum = new Random();
         //Reference [4]
@@ -71,7 +74,32 @@ public class Main {
     }
 
     public static void linkedListMethod() {
-        System.out.println("Sample code.");
+        //New method for LinkedList
+
+        //Declaring the LinkedList
+        LinkedList<Integer> myLinkedList = new LinkedList<Integer>();
+        //Reference [7]
+
+        //New method for random integers
+        Random randNum = new Random();
+        //Reference [4]
+
+        //New upper limit integer variable for data structures
+        int numLimit = 2000;
+
+        //Appending new elements at the end of the list
+        for (int i = 0; i < numLimit; i++) {
+            myLinkedList.add(randNum.nextInt());
+            //System.out.println("Index: " + i + " Int value added" + " " + "Array size: " + myLinkedList.size()); //Simple test to check for array population. Array list populates to 2mil.
+        }
+        //Reference [4],[7]
+
+        //Deleting elements of the ArrayList
+        for (int i = numLimit - 1; myLinkedList.size() > 0; i--) {
+            myLinkedList.remove(i);
+            System.out.println("Index: " + i + " | Int value removed" + " | LinkedList size: " + myLinkedList.size() + " | LinkedList value: " + myLinkedList); //Simple test to check for array value deletion.
+        }
+        //Reference [5]
     }
 
     public static void hashtableMethod() {
@@ -94,5 +122,6 @@ References:
 [4] “Generating random numbers in Java,” GeeksforGeeks, Oct. 26, 2016. https://www.geeksforgeeks.org/generating-random-numbers-in-java/ (accessed Sep. 24, 2023).
 [5] “ArrayList and LinkedList remove() methods in Java with Examples,” GeeksforGeeks, Nov. 06, 2016. https://www.geeksforgeeks.org/arraylist-linkedlist-remove-methods-java-examples/ (accessed Sep. 24, 2023).
 [6] codebox, “Answer to ‘Exception in thread “main” java.lang.ArrayIndexOutOfBoundsException: 1000000 at problem2.main(problem2.java:17),’” Stack Overflow, Aug. 17, 2012. https://stackoverflow.com/a/12005428 (accessed Sep. 24, 2023).
+[7] “LinkedList element() method in Java with Examples,” GeeksforGeeks, Oct. 11, 2018. https://www.geeksforgeeks.org/linkedlist-element-method-in-java-with-examples/ (accessed Sep. 24, 2023).
 
 */
